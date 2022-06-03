f969fba6fb2b96ae95f744b9194bb51515048a65
aviv12234 <58222380+aviv12234@users.noreply.github.com>
2022-06-03 19:58:18 +0300

    gaga

--- Deserter/Assets/HealthBar.cs
+++ Deserter/Assets/HealthBar.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
 
     // Start is called before the first frame update
+
+    public Slider slider;
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
     void Start()
     {
-        transform.localscale = new Vector2(2, 2);
     }
 
     // Update is called once per frame
