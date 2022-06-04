c1e3a127d45d21bf120e31582f07aa9cf700f0f4
aviv12234 <58222380+aviv12234@users.noreply.github.com>
2022-06-04 15:22:53 +0300

    baba

---
+++ Deserter/Assets/Selected.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selected : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("terrian");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
